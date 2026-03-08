2b0a215582c2a49a8bed38ec7565678d4ebcc275
Andrew Peterson <andrewp2@umbc.edu>
2026-03-08 16:59:04 -0400

    small refactor for theme switcher

--- src/app/components/theme-switcher/theme-switcher.ts
+++ src/app/components/theme-switcher/theme-switcher.ts
@@ -11,7 +11,18 @@ export class ThemeSwitcher implements OnInit {
   isLight = signal(true);
 
   ngOnInit(): void {
-    const theme = localStorage.getItem("dpc_theme");
+    this.getStoredTheme();
+  }
+  toggleTheme() {
+    this.isLight.set(this.isLight() === true ? false : true);
+
+    localStorage.setItem(
+      "pdc_theme",
+      this.isLight() === true ? "light" : "dark",
+    );
+  }
+  getStoredTheme() {
+    const theme = localStorage.getItem("pdc_theme");
     if (theme === "light") {
       console.log("found light theme in local storage");
       this.isLight.set(true);
@@ -19,12 +30,5 @@ export class ThemeSwitcher implements OnInit {
       console.log("found dark theme in local storage");
       this.isLight.set(false);
     }
-  }
-  toggleTheme() {
-    this.isLight.set(this.isLight() === true ? false : true);
-    localStorage.setItem(
-      "dpc_theme",
-      this.isLight() === true ? "light" : "dark",
-    );
   }
 }
