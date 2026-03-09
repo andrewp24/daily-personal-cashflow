9e80b38b35bb26b97ce8e0db13e2821eec4fb1fb
Andrew Peterson <andrewp2@umbc.edu>
2026-03-09 12:59:54 -0400

    attempt 1

--- src/app/pages/inflows/inflows.ts
+++ src/app/pages/inflows/inflows.ts
@@ -1,9 +1,44 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { InflowData } from "./interfaces";
+import {
+  form,
+  FormField,
+  pattern,
+  required,
+  validate,
+} from "@angular/forms/signals";
+import { CurrencyPipe, NgClass } from "@angular/common";
 
+const EDITING_AMOUNT_REGEX = /^\d*\.?\d{0,2}$/;
 @Component({
   selector: "app-inflows",
-  imports: [],
+  imports: [FormField, CurrencyPipe, NgClass],
   templateUrl: "./inflows.html",
   styleUrl: "./inflows.css",
 })
-export class Inflows {}
+export class Inflows {
+  constructor() {}
+
+  inflowModel = signal<InflowData>({
+    cashOnHand: "",
+    paycheckAmount1: "",
+  });
+  inflowForm = form(this.inflowModel, (schemaPath) => {
+    //required(schemaPath.cashOnHand, { message: "Cash on hand is required." });
+    pattern(schemaPath.cashOnHand, /^(0|[1-9]\d*)(\.\d{1,2})?$/, {
+      message:
+        "Must not have more than 2 decimal places. and no commas or currency symbols. Also no leading zeros unless the value is zero or has decimal places.",
+    });
+    pattern(schemaPath.paycheckAmount1, /^(0|[1-9]\d*)(\.\d{1,2})?$/, {
+      message:
+        "Must not have more than 2 decimal places. and no commas or currency symbols. Also no leading zeros unless the value is zero or has decimal places.",
+    });
+  });
+
+  totalAmount = parseFloat(
+    this.inflowForm
+      .cashOnHand()
+      .value()
+      .replace(/[^0-9.]/g, ""),
+  );
+}
---
+++ src/app/pages/inflows/interfaces.ts
@@ -0,0 +1,8 @@
+export interface InflowData {
+  cashOnHand: string;
+  paycheckAmount1: string;
+  paycheckAmount2?: number;
+  paycheckAmount3?: number;
+  paycheckAmount4?: number;
+  paycheckAmount5?: number;
+}
