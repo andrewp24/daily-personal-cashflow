36695644d978033d239a5dd2ffe58d4f3083a3f4
Andrew Peterson <andrewp2@umbc.edu>
2026-03-19 06:30:00 -0400

    make file export/import the only option for saving data

--- src/app/pages/settings/settings.ts
+++ src/app/pages/settings/settings.ts
@@ -29,31 +29,6 @@ export class Settings implements OnDestroy {
     }
   }
 
-  exportOutflowsInflows(): string {
-    try {
-      const data = {
-        inflow_data: localStorage.getItem("pdc_inflows"),
-        outflow_data: localStorage.getItem("pdc_outflows"),
-      };
-      const encoded = btoa(JSON.stringify(data))
-        .replace(/\+/g, "-")
-        .replace(/\//g, "_")
-        .replace(/=/g, "");
-      navigator.clipboard.writeText(encoded);
-      this.exportSuccess.set("Successfully copied to clipboard.");
-      return "success";
-    } catch (error) {
-      this.exportFailure.set("Failed to copy data to clipboard.");
-      console.error(error);
-      return "failed";
-    } finally {
-      this.timeoutId = setTimeout(() => {
-        this.exportSuccess.set(null);
-        this.exportFailure.set(null);
-      }, 3000);
-    }
-  }
-
   exportToFile(): void {
     try {
       const data: SaveFile = {
@@ -160,30 +135,5 @@ export class Settings implements OnDestroy {
       }, 3000);
     };
     reader.readAsText(file);
-  }
-
-  async importOutflowsInflows(): Promise<string> {
-    try {
-      const encoded = await navigator.clipboard.readText();
-      console.log(encoded);
-      const padded = encoded.replace(/-/g, "+").replace(/_/g, "/");
-      const data = JSON.parse(atob(padded));
-      console.log(data);
-      if (data.inflow_data)
-        localStorage.setItem("pdc_inflows", data.inflow_data);
-      if (data.outflow_data)
-        localStorage.setItem("pdc_outflows", data.outflow_data);
-      this.importSuccess.set("Successfully import data from clipboard.");
-      return "success";
-    } catch (error) {
-      this.importFailure.set("Failed to import data from clipboard.");
-      console.error(error);
-      return "failed";
-    } finally {
-      this.timeoutId = setTimeout(() => {
-        this.importSuccess.set(null);
-        this.importFailure.set(null);
-      }, 3000);
-    }
   }
 }
